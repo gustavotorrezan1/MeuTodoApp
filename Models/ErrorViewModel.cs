bbdb1a6e56f9d2192e103ebafb3413b2e6b0b017
gustavotorrezan1 <gustorrezani@gmail.com>
2023-08-16 10:44:54 -0300

    .

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MeuTodoApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/Todo.cs
@@ -0,0 +1,11 @@
+﻿namespace MeuTodoApp.Models;
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Titulo { get; set; }
+    public bool Feito { get; set; }
+    public DateTime Criacao { get; set; } = DateTime.Now;
+    public DateTime UltimaModificacao { get; set; } = DateTime.Now;
+    public string Usuario { get; set; }
+}
